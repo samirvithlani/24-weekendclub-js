ba320ddf394aa76c57535b4a0c7f114ca31ec11f
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-31 10:28:18 +0530

    json

---
+++ json/json1.js
@@ -0,0 +1,29 @@
+// var sid = 101
+// ....
+
+var student = {
+    sid:101,
+    sname:"Ravi",
+    sage:23,
+    sfees:1200,
+    address:{
+       city:"Bangalore",
+        state:"Karnataka",
+        pincode:560001,
+        street:"MG Road",
+    }
+}
+
+console.log(student)
+console.log(student.sid)
+console.log(student.sname)
+console.log(student.sage)
+console.log(student.sfees)
+
+console.log(student.address)
+console.log(student.address.city)
+
+// console.log(student.city)
+// console.log(student.state)
+// console.log(student.pincode)
+// console.log(student.street)
