3c55fb41e9498c97aae3693be0c7f177c5b6f420
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-07 10:36:42 +0530

    indexof

---
+++ string/string2.js
@@ -0,0 +1,7 @@
+var data = "Amit";
+
+console.log(data.codePointAt(0))
+
+
+//var statment = "india is beautiful country"
+
---
+++ string/string3.js
@@ -0,0 +1,6 @@
+var mystr = "ganesha";
+
+
+//var ind = mystr.indexOf("a")
+var ind = mystr.lastIndexOf("a")
+console.log(ind) 
