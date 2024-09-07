94bc045f406ff9cc620e1f33be16aec32df12658
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-07 11:20:36 +0530

    substring

--- string/string3.js
+++ string/string3.js
@@ -4,3 +4,14 @@ var mystr = "ganesha";
 //var ind = mystr.indexOf("a")
 var ind = mystr.lastIndexOf("a")
 console.log(ind) 
+
+
+//var x = mystr.substring(0,5)
+//var x = mystr.substring(1)
+//var x = mystr.slice(1,5)
+//var x = mystr.slice(2)
+console.log(x)
+
+
+// var sp = 1
+// var ep = 2
