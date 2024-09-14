3d3b41d6ea5698ec2cc16a6f0b23d43c6501287b
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-14 10:27:07 +0530

    uppercase

---
+++ string/string5.js
@@ -0,0 +1,17 @@
+var data = "AMIT"
+var x = data.charCodeAt(0)
+console.log(x)
+
+var x1 = String.fromCharCode(65)
+console.log(x1)
+
+
+var x2 = String.fromCharCode(x)
+console.log(x2)
+
+var x3 = String.fromCharCode(data.charCodeAt(0)+32)
+console.log(x3)
+
+
+//var userName = "ram"
+//var upperName = "RAM"
