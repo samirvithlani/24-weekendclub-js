8f5e7341892918750c50e9344d9937fabf504c98
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-28 10:54:50 +0530

    array push

---
+++ array/array5.js
@@ -0,0 +1,31 @@
+var lang = ["Gujarati","Hindi","English","Marathi"];
+var upperLang=[];
+
+
+for(let i=0;i<lang.length;i++){
+
+    //[] = lang[i].toUpperCase();
+    upperLang.push(lang[i].toUpperCase())
+
+}
+
+console.log(upperLang);
+
+var numbers = [10,11,34,55,65,34,12,34,56,78,98,99,121,73];
+//sum=[1,2,7,10,11,7]]
+//even[]
+//odd[]
+
+//sum of digits
+
+
+for(let i=0;i<numbers.length;i++){
+
+    //10
+    while(numbers[i]){
+
+    }
+    //push
+
+}
+
