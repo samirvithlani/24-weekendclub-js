e69f4af37379db85e84f0e714315af3c22cfd058
samirvithlani <samir.vithlani83955@gmail.com>
2024-11-17 12:41:55 +0530

    function3

---
+++ function/function3.js
@@ -0,0 +1,40 @@
+//with return type without arguments
+
+function test(){
+    console.log("test function called....")
+    return 100
+}
+
+
+var x = test()
+console.log("x = ",x)
+
+function add(){
+
+
+    let a = 10,b=20,c=0;
+    c = a + b;
+    return c;
+
+}
+
+var ans = add()
+console.log("ans = ",ans)
+
+
+//function time: 12:00
+
+function greetings(){
+    
+
+    return "Good Morning"
+}
+
+
+var greet = greetings()
+console.log("greet = ",greet)
+
+
+
+
+
