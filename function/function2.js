5d77b40c5e4cc63c0788df927f06f0004c9891e7
samirvithlani <samir.vithlani83955@gmail.com>
2024-11-17 12:13:32 +0530

    function

---
+++ function/function2.js
@@ -0,0 +1,24 @@
+//without return type with patameter | arguments
+
+//let a , let b
+function add(a,b){
+
+    console.log("a = ",a)
+    console.log("b = ",b)
+    let c = a + b;
+    console.log("ans = ",c)
+}
+
+add(100,200) // calling part
+//add() // calling part it will not throw any errror...
+//add(100)
+
+function greetings(name){
+    console.log("Hello ",name)
+}
+
+greetings("Ravi")
+greetings("Ramesh")
+
+
+//pass number to function and find sum of difits...
