9774c1ebaa7c3cee5481abab427ced53384d3e66
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-08 10:41:26 +0530

    div

--- events/event1.js
+++ events/event1.js
@@ -6,3 +6,32 @@ const changeText =()=>{
     txt.style.color="red"
 
 }
+
+const changeLink = ()=>{
+
+    const google = document.getElementById("google")//<a>
+    google.href = "https://www.hotstar.com"
+    google.innerHTML ="hotstar"
+}
+
+var count = 0
+const changeShape =()=>{
+
+    count++
+    if(count==1){
+        const mydiv = document.getElementById("mydiv") //<div>
+        mydiv.style.borderRadius = "50%"
+        mydiv.style.backgroundColor="blue"
+    }
+    else if(count==2){
+        
+        const mydiv = document.getElementById("mydiv") //<div>
+        mydiv.style.borderRadius = "0%"
+        mydiv.style.height="200px"
+        mydiv.style.width="200px"
+        
+        //mydiv.style.borderRadius = "50%"
+        //mydiv.style.backgroundColor="blue"
+    }
+
+}
