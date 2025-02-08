7dbcc4a1e3eb6167ff09b7f9a1d580f2bf02a25c
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-08 10:26:15 +0530

    onclick

---
+++ events/event1.js
@@ -0,0 +1,8 @@
+const changeText =()=>{
+    //alert("function called...")
+    const txt = document.getElementById("txt") //<h1></h1>
+    //console.log(txt.innerHTML)
+    txt.innerHTML = "HI"
+    txt.style.color="red"
+
+}
