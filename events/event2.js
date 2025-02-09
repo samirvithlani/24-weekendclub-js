660c6cf1d81c59c0ad748650dc96571a7fb548e7
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-09 11:27:41 +0530

    random

---
+++ events/event2.js
@@ -0,0 +1,15 @@
+
+var count =0;
+const changeColor =()=>{
+    count++;
+    //console.log("called...",count)
+    //console.log("function called....")
+    //const mydiv = document.getElementById("mydiv")
+    //mydiv.style.backgroundColor="green"
+    const rno = Math.floor(Math.random()*7)    
+console.log(rno)
+}
+
+
+//array = colors ==>[]
+
