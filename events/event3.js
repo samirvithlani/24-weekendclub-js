9e8e08d228c8d55cf9686ce42a9c67813ffc8211
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-16 11:41:53 +0530

    onleyup[

--- events/event3.js
+++ events/event3.js
@@ -1,5 +1,27 @@
-const getData =() =>{
+// const getData =() =>{
 
+//     const txt = document.getElementById("txt") //<input>
+//     console.log(txt.value)
+// }
+
+const users =["ram","ramesh","ramila","sumit","sushma","sneha","sumita"]
+
+const searchData =()=>{
     const txt = document.getElementById("txt") //<input>
     console.log(txt.value)
+
+    var filtUser = users.filter((user)=>{
+        return user.includes(txt.value)
+    })
+
+    console.log(filtUser)
+
 }
+
+// var users = [
+//     {
+//         id:
+//         fname:
+//         lname:
+//     }
+// ]
