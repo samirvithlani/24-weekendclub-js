14a1602ec1eaee06303b3ca312e1da88600dff9d
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-23 11:56:57 +0530

    radio checkbox

--- events/form/formdemo1.js
+++ events/form/formdemo1.js
@@ -12,4 +12,24 @@ const submitHandler = (event)=>{
     console.log(age.value)
     ageOutput.innerHTML = "Age = "+age.value
 
+
+    const gender = document.getElementsByName("gender") //<input>,<input> array
+    for(let i=0;i<gender.length;i++){
+
+        if(gender[i].checked){
+            console.log(gender[i].value)
+        }
+    }
+
+    const hobbies = document.getElementsByName("hobbies")
+
+    for(let i=0;i<hobbies.length;i++){
+
+        if(hobbies[i].checked){
+            console.log(hobbies[i].value)
+        }
+    }
+
+
+
 }
